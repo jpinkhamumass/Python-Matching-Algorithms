0543196ed17a35a280c1abed5ed14a92c24b34c7
jpinkhamumass <150302347+jpinkhamumass@users.noreply.github.com>
2025-02-13 14:20:54 -0500

    functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,21 @@
 #testing123
-def test(code):
+def test_func():
     print("hello")
-#test3
+
+# Hi my name is james
+def test_func2():
+    print("hello2")
+
+test_func()
+test_func2()
+
+#testing123
+
+def test_a():
+    print("hello akhila")
+
+#jamals_test
+print("hello")
+
+def test_func3():
+    print("hello, world, hi, there,")
