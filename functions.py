16a1edd64b44542a205b8a32665b30776fe0a1c1
jpinkhamumass <jpinkham@umass.edu>
2025-02-13 14:09:17 -0500

    Co-authored-by: garreakhila <garreakhila@users.noreply.github.com> Co-authored-by: Jishnu Subramanian <jsubramanian@umass.edu> Co-authored-by: jamalsgit <jamalsgit@users.noreply.github.com>

--- functions.py
+++ functions.py
@@ -3,3 +3,19 @@ def test_func():
     print("hello")
 
 # Hi my name is james
+def test_func2():
+    print("hello2")
+
+test_func()
+test_func2()
+
+#testing123
+
+def test_a():
+    print("hello akhila")
+
+#jamals_test
+print("hello")
+
+def test_func3():
+    print("hello, world, hi, there,")
