721de787c3c5ee0135489c8c2d040edd87fb5f93
jamalsgit <jamkarimli@gmail.com>
2025-03-03 21:45:14 -0500

    Update ttc.py

--- ttc.py
+++ ttc.py
@@ -1,12 +1,15 @@
 import csv
 from Matching_Algorithms import csvReader
-people = csvReader("people.csv")
 
-for person in people.values():
-    print(person.preferences)
-#hjdf
+people = Matching_Algorithms.csvReader('people.csv')
+preferences = Preference_Base.csvReader('preferences.csv')
 
-def hi():
-    return
+def top_trading_cycles(peopleDict, preferencesDict):
+    outputMatches = {}
+    while peopleDict:
+        
+
+
+
 def ttc(all):
     num_agents = len(all)
