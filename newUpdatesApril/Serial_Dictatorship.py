7039917eeac97399ade1b47e639c109b6612077a
jpinkhamumass <150302347+jpinkhamumass@users.noreply.github.com>
2025-04-08 13:15:58 -0400

    Update Serial_Dictatorship.py

--- newUpdatesApril/Serial_Dictatorship.py
+++ newUpdatesApril/Serial_Dictatorship.py
@@ -1,45 +1,43 @@
 import Matching_Algorithms; import csv; import random; import math; import Preference_Base
 
+# Creates a dictionary of people and their preferences
 peopleDictionary = Matching_Algorithms.csvReader('people.csv')
 
+#Initialize a boolean random to be input later 
 randomBoolean = "False"
 
 def randomness():
     global randomBoolean  # Declare randomBoolean as global
     inputGetter = input("Random: True or False: ") # True or False
 
-    if inputGetter == 'True':
+    if inputGetter == 'True': # Changes method to use an arbitrary order
         randomBoolean = inputGetter
         print("successfully set to True.")
         return
 
-    if inputGetter == 'False':
+    if inputGetter == 'False': # Uses the order from input
         print("successfully set to False.")
         return
 
-    else:
+    else: # Invalid input
         print("Error: Please enter True or False: ")
         randomness()
 
-def count(peopleDictionary):
-    count = 0
-    for person in peopleDictionary.keys():
-            count += 1
-    return count
+count = len(peopleDictionary) # Number of people in input
 
-def iDNumberListMaker(peopleDictionary):
+def iDNumberListMaker(peopleDictionary): # Makes a list of people ID's
     IDNumberList = []
     for person in peopleDictionary.keys():
         IDNumberList.append(person)
     return IDNumberList
 
-iDNumberList = iDNumberListMaker(peopleDictionary)
+iDNumberList = iDNumberListMaker(peopleDictionary) # Makes a list of people ID's from input
 
-randomness()
+randomness() # User determines if input order random or not
 
 
 
-if randomBoolean == "True":
+if randomBoolean == "True": 
      # This shuffler will give us the random order in which agents will choose their objects of preference
     random.shuffle(iDNumberList)
 
@@ -48,23 +46,23 @@ if randomBoolean == "True":
 #Algorithm 
 def output(file):
 
-    preferenceDictionary = Preference_Base.csvReader(file)
+    preferenceDictionary = Preference_Base.csvReader(file) 
     outputList = {}
 
-    for i in iDNumberList:
-        personOfInterest = peopleDictionary[i].preferences.values()
-        for j in personOfInterest:
-            if preferenceDictionary[j] > 0:
-                preferenceDictionary[j] -= 1
-                peopleDictionary[i].actualMatch = j
-                outputList[peopleDictionary[i].IDNumber] = j
+    for i in iDNumberList: # per person
+        personOfInterest = peopleDictionary[i].preferences.values() # gets persons preferences
+        for j in personOfInterest: # per preference
+            if preferenceDictionary[j] > 0: # preference still has avaiability
+                preferenceDictionary[j] -= 1 # decrements number of items that can be allocated
+                peopleDictionary[i].actualMatch = j # updates match
+                outputList[peopleDictionary[i].IDNumber] = j # updates output list
                 break
             else:
                 continue
 
     return list(outputList.items())
 
-def printer(file):
+def printer(file): 
     print(output(file))
 
 printer('preferences.csv')
