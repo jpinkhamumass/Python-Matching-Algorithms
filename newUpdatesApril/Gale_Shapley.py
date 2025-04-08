5d23be7a4fdd72ab647da4eeb2967bf697996ce6
Jishnu Subramanian <jsubramanian@umass.edu>
2025-04-08 13:21:08 -0400

    Update Gale_Shapley.py

--- newUpdatesApril/Gale_Shapley.py
+++ newUpdatesApril/Gale_Shapley.py
@@ -2,6 +2,7 @@ import csv
 
 #This code is a csv reader for the Gale-Shapley algorithm, which reads in the preferences of the 2 groups (i.e. male and female) within the code.
 def csvReader_gale(file):
+    
     #The preferenceDict is a dictionary that will hold the preferences of each individual. The key is the Individual's ID and the value is a list of their preferences.
     preferenceDict = {} 
     try:
@@ -11,6 +12,7 @@ def csvReader_gale(file):
                 IndividualID = row[0]
                 individualPreferences = row[1:]
                 preferenceDict[IndividualID] = individualPreferences
+   
     except FileNotFoundError:
         print(f"Error: The file {file} was not found or does not have the proper formatting.")
     
@@ -27,6 +29,7 @@ def gale_shapley(prefGroup1, prefGroup2):
 
     # While there are still unmatched individuals, this loop will continue to work so all individuals/agents are matched
     while available: 
+        
        # The first individual in the available list is selected
        # and their preferences are retrieved from the preference dictionary
         individual1 = available.pop(0)
@@ -39,7 +42,6 @@ def gale_shapley(prefGroup1, prefGroup2):
 
             #This line retrieves the preferences of individual 2 from their preference dictionary
             # The pair variable is used to check if individual2
-
             individual2preferences = prefGroup2[individual2]
             pair = currentMatches.get(individual2)
 
@@ -54,6 +56,7 @@ def gale_shapley(prefGroup1, prefGroup2):
                     currentMatches[individual2] = individual1
                     available.append(pair)
                     break
+                    
     # Returns a tuple of the current matches
     return list(currentMatches.items())
 
@@ -72,6 +75,8 @@ def pairs_stable(men, women, men_prefs, women_prefs):
 
 def get_pref(actor, target, preferences):
     return preferences[actor].index(target)
+
+
 
 
 #Examples using all the function and specific CSVs
